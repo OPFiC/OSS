c0fc0257591ed62430c21a1b3e49ac58f823881b
opfic <0412justin1@gmail.com>
2023-03-09 16:50:15 +0900

    배열을 이용한 큐 구현

---
+++ OSS-Swift/Queue.playground/Contents.swift
@@ -0,0 +1,45 @@
+class Queue{
+    var arr:[Any]
+    init(){self.arr=[]}
+    
+    func push(i:Any){
+        self.arr=[i]+self.arr
+    }
+    func pop()->Any?{
+        if self.arr.count==0{return nil}
+        return self.arr.removeLast()
+    }
+    func clear(){
+        self.arr.removeAll()
+    }
+    
+    func isempty()->Bool{
+        if self.arr.count==0{return true}
+        else {return false}
+    }
+    
+    func size()->Int{
+        return self.arr.count
+    }
+    func showAll()->Array<Any>{
+        return self.arr
+    }
+    
+}
+
+var q1:Queue=Queue()
+print(q1.isempty())
+q1.push(i:1)
+q1.push(i:2)
+q1.push(i:3)
+q1.push(i:4)
+print(q1.showAll())
+print(q1.pop())
+print(q1.pop())
+print(q1.showAll())
+print(q1.size())
+print(q1.isempty())
+q1.clear()
+print(q1.isempty())
+print(q1.showAll())
+
